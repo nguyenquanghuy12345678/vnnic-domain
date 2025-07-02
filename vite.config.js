fdee310c9ed4d37607776ee8b56ea5fad548d2cc
nguyenquanghuy12345678 <huynq.23ns@vku.udn.vn>
2025-07-02 17:51:20 +0700

    fix location file deploy

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
   publicDir: 'public',
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: './public/index.html',
-      },
-    },
   },
 });
